ece3f4d48c57ab1754d7d608922699f30e55a095
kooistradev <info@kooistra-development.nl>
2022-03-09 15:33:10 +0100

    Add project files.

---
+++ JwtWebApiTutorial/User.cs
@@ -0,0 +1,12 @@
+﻿namespace JwtWebApiTutorial
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+    }
+}
